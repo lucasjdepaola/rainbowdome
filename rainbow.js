bc620cdf0c9f18e8c036e8b7bd7df9c05b94d692
LD211 <93799505+LD211@users.noreply.github.com>
2023-10-03 10:42:16 -0400

    automated update.

--- rainbow.js
+++ rainbow.js
@@ -1,4 +1,4 @@
-const colors = new Map();
+const colors = {};
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //!@#$%^&*()-
 colors["black"] = 30;
 colors["red"] = 31;
@@ -17,14 +17,8 @@ colors["bmagenta"] = 95;
 let rainbowcolors = ["bred", "byellow", "bgreen", "bmagenta", "bcyan"];
 function clear() {
   console.clear();
-  // process.stdout.write('\x1b[2J\x1b[H');
 }
 clear();
-
-function setPosition(row, col, color, char) {
-  process.stdout.write(`\x1b[${row};${col}H`);
-  //\x1b[]
-}
 
 function displayRainbowCharacters(color, value, row, col, dim) {
   const position = `\x1b[${row + 1};${col + 1}H`;
@@ -42,14 +36,11 @@ function createRandomCharArray(row, col, dim) {
   );
 }
 
-async function displayMatrix(dim) {
+function displayMatrix(dim) {
   for (let row = 0; row < 30; row++) {
     for (let col = 0; col < 135; col += 2) {
       process.stdout.write(createRandomCharArray(row, col, dim));
     }
-    // if(dim === "") {
-    //   await sleep(.0000001);
-    // }
   }
 }
 
@@ -74,13 +65,14 @@ async function displayRandomMatrix() {
   }
 }
 
-async function sleep(ms) {
+function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms * 1000));
 }
 
 function randomNum(max) {
   return Math.floor(Math.random() * max);
 }
+
 function clearCursor() {
   console.log("\x1b[?25l\x1b");
 }
@@ -89,20 +81,13 @@ function showCursor() {
   console.log("\x1b[?25h\x1b");
 }
 
+process.on("SIGINT", () => {
+  console.clear();
+  console.log("keyboard has interrupted, quitting now.");
+  showCursor();
+  process.exit();
+});
+
 // 2m for dim mode
 
-async function displayLoop() {
-  clearCursor();
-  clear();
-  while (true) {
-    await displayMatrix("");
-    await sleep(.15);
-    await displayMatrix("\x1b[2m");
-    await sleep(.02);
-  }
-}
 displayRandomMatrix();
-
-// displayLoop();
-// process.stdout.write(`\x1b[${100};${10}H`);
-// process.stdout.write("l");
